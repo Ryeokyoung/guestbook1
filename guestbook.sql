d81824c9c8dafd0fce7a299035e5e84709b81d3c
Ryeokyoung <ryeokyoungk@gmail.com>
2022-05-27 15:59:16 +0900

    2022-05-27

---
+++ guestbook.sql
@@ -0,0 +1,38 @@
+create table guestbook  		no       number(),
+						        name      varchar2(80),
+						        password  varchar2(20),
+						        content   varchar2(2000),
+						        reg_date  date,
+                                primary key (no); 
+                                
+/*ORA-00933: SQL command not properly ended
+00933. 00000 -  "SQL command not properly ended"  문장끝이 이상함  */   
+                                
+                                
+create table guestbook(  		no       number,
+						        name      varchar2(80),
+						        password  varchar2(20),
+						        content   varchar2(2000),
+						        reg_date  date
+                                primary key (no)); 
+/*ORA-00907: missing right parenthesis
+00907. 00000 -  "missing right parenthesis" 콤마없어 이자식아 */
+                                
+                                
+
+create table guestbook(  		no       number,
+						        name      varchar2(80),
+						        password  varchar2(20),
+						        content   varchar2(2000),
+						        reg_date  date,
+                                primary key (no)); 
+/*정상작동...하하.. */                                
+
+
+                                
+                                
+select *
+from guestbook;
+
+
+commit;
